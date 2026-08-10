aab1069d44180f0af801bc6a5df3e8a8f68e9732
diajxkfskck-oss <diajxkfskck@gmail.com>
2026-08-10 15:59:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "31280586"))
 API_HASH = environ.get("API_HASH", "5f30188e8ff92b134ea343bb01eeaa90")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8852786935:AAGwiBi2KF4LTOmq9GvyfYes0qW58jJBuYA")
